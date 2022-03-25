c825bd063d1798c6542d6aca3ee80566fe84f631
moohat <refixzcorp@gmail.com>
2022-03-25 08:13:53 -0700

    set dummy data using usesState

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,16 +1,45 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {ListDoctor} from '../../components';
 import {colors, fonts} from '../../utils';
+import {DummyDoctor4, DummyDoctor5, DummyDoctor6} from '../../assets';
 
 const Messages = () => {
+  const [doctors, setDoctors] = useState([
+    {
+      id: 1,
+      name: 'Alexander Jennie',
+      desc: 'kumaha dok, jadinya bagaimana...',
+      profile: DummyDoctor4,
+    },
+    {
+      id: 2,
+      name: 'Nairobi Putri Hayza',
+      desc: 'Oh tentu saja tidak karena jeruk it...',
+      profile: DummyDoctor5,
+    },
+    {
+      id: 3,
+      name: 'John McParker Steve',
+      desc: 'Oke menurut pak dokter bagaimana unt...',
+      profile: DummyDoctor6,
+    },
+  ]);
+
   return (
     <View style={styles.page}>
       <View style={styles.content}>
         <Text style={styles.title}>Messages</Text>
-        <ListDoctor />
-        <ListDoctor />
-        <ListDoctor />
+        {doctors.map(doctor => {
+          return (
+            <ListDoctor
+              key={doctor.id}
+              profile={doctor.profile}
+              name={doctor.name}
+              desc={doctor.desc}
+            />
+          );
+        })}
       </View>
     </View>
   );
