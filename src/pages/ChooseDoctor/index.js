69dc9e566dab5281cf3fb6f3df963f7dc2ecece7
moohat <refixzcorp@gmail.com>
2022-04-04 09:13:48 -0700

    add doctor page

---
+++ src/pages/ChooseDoctor/index.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ChooseDoctor = () => {
+  return (
+    <View>
+      <Text>ChooseDoctor</Text>
+    </View>
+  );
+};
+
+export default ChooseDoctor;
+
+const styles = StyleSheet.create({});
