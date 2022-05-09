9040e3ced5e96335beac736121f0631e02a0620a
moohat <refixzcorp@gmail.com>
2022-05-09 09:08:55 -0700

    implementasi login dengan firebase

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,30 +1,92 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import {ILLogo} from '../../assets';
-import {Button, Gap, Input, Link} from '../../components';
-import {colors, fonts} from '../../utils';
+import {Button, Gap, Input, Link, Loading} from '../../components';
+import {colors, fonts, useForm, storeData} from '../../utils';
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import {Fire} from '../../config/Fire';
+import {getDatabase, ref, child, get} from 'firebase/database';
+import {showMessage} from 'react-native-flash-message';
 
 const Login = ({navigation}) => {
-  return (
-    <View style={styles.page}>
-      <ILLogo />
-      <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
-      <Input label="Email Address" />
-      <Gap height={24} />
-      <Input label="Password" />
-      <Gap height={10} />
-      <Link title="Forgot My Password" size={12} />
-      <Gap height={40} />
-      <Button title="Sign In" onPress={() => navigation.replace('MainApp')} />
-      <Gap height={30} />
+  const [form, setForm] = useForm({
+    email: '',
+    password: '',
+  });
+  const [loading, setLoading] = useState(false);
 
-      <Link
-        title="Crate New Account"
-        size={16}
-        align="center"
-        onPress={() => navigation.replace('Register')}
-      />
-    </View>
+  const login = () => {
+    setLoading(true);
+    const auth = getAuth(Fire);
+    const dbRef = ref(getDatabase(Fire));
+    signInWithEmailAndPassword(auth, form.email, form.password)
+      .then(res => {
+        setLoading(false);
+        // setForm('reset');
+        // console.log('form: ', res);
+        get(child(dbRef, `users/${res.user.uid}`)).then(resDB => {
+          if (resDB.exists()) {
+            console.log('data user: ', resDB.val());
+            storeData('user', resDB.val());
+            navigation.replace('MainApp');
+          } else {
+            console.log('No data available');
+          }
+        });
+      })
+
+      .catch(error => {
+        setLoading(false);
+
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('error Login: ', errorMessage);
+        showMessage({
+          message: errorMessage,
+          description: 'This is our second message',
+          type: 'danger',
+          color: colors.white,
+        });
+        // ..
+      });
+  };
+
+  return (
+    <>
+      <View style={styles.page}>
+        <ScrollView showsHorizontalScrollIndicator={false}>
+          <Gap height={40} />
+
+          <ILLogo />
+          <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
+          <Input
+            label="Email Address"
+            value={form.email}
+            onChangeText={value => setForm('email', value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Password"
+            value={form.password}
+            onChangeText={value => setForm('password', value)}
+            secureTextEntry
+          />
+          <Gap height={10} />
+          <Link title="Forgot My Password" size={12} />
+          <Gap height={40} />
+          <Button title="Sign In" onPress={login} />
+          <Gap height={30} />
+
+          <Link
+            title="Crate New Account"
+            size={16}
+            align="center"
+            onPress={() => navigation.replace('Register')}
+          />
+        </ScrollView>
+      </View>
+      {loading && <Loading />}
+    </>
   );
 };
 
