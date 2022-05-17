683e76c75e2fa92ed9596d7da96c2d36a3c56096
moohat <refixzcorp@gmail.com>
2022-05-17 09:08:37 -0700

    setup redux

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import Router from './router';
 import FlashMessage from 'react-native-flash-message';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 const App = () => {
   return (
-    <>
-    
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
-    <FlashMessage position="top" /> 
-    </>
-
+    <Provider store={store}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+      <FlashMessage position="top" />
+    </Provider>
   );
 };
 
---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import {createStore} from 'redux';
+
+const initialState = {
+  loading: false,
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+const store = createStore();
+export default store;
