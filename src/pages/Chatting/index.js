630aaa9710e5b0f98ec4fbce2955bf6d7672fd20
moohat <refixzcorp@gmail.com>
2022-06-08 07:47:36 -0700

    integrasi chatting

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -1,10 +1,59 @@
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, ChatItem, Header, Input, InputChat} from '../../components';
-import {colors, fonts} from '../../utils';
+import {colors, fonts, getData, showError} from '../../utils';
+import {Fire} from '../../config/Fire';
+import {child, get, getDatabase, push, ref} from 'firebase/database';
 
 const Chatting = ({navigation, route}) => {
   const dataDoctor = route.params;
+  const [chatContent, setChatContent] = useState('');
+  const [user, setUser] = useState({});
+  const dbRef = ref(getDatabase(Fire));
+
+  useEffect(() => {
+    getData('user').then(res => {
+      // console.log('data user login: ', res);
+      setUser(res);
+    });
+  }, []);
+
+  const chatSend = () => {
+    console.log('data dikirim : ', chatContent);
+    const today = new Date();
+    const hour = today.getHours();
+    const minute = today.getMinutes();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+    //kirim ke firebase
+    // console.log('url ke firebase: ', `chatting/${user.uid}_${dataDoctor.data.uid}/allChat/${year}-${month}-${date}`);
+    const data = {
+      sendBy: user.uid,
+      chatDate: new Date().getTime(),
+      chatTime: `${hour}:${minute} ${hour > 12 ? 'PM' : 'AM'}`,
+      chatContent: chatContent,
+    };
+    // dbRef(
+    //   `chatting/${user.uid}_${dataDoctor.data.uid}/allChat/${year}-${month}-${date}`,
+    //   push(data),
+    // );
+    push(
+      child(
+        dbRef,
+        `chatting/${user.uid}_${dataDoctor.data.uid}/allChat/${year}-${month}-${date}`,
+      ),
+      data,
+    )
+      .then(res => {
+        setChatContent('');
+      })
+      .catch(err => {
+        showError(err.message);
+      });
+
+    console.log('data untuk dikirim: ', data);
+  };
 
   return (
     <View style={styles.page}>
@@ -25,9 +74,9 @@ const Chatting = ({navigation, route}) => {
       </View>
 
       <InputChat
-        value=""
-        onChangeText={() => alert('input text')}
-        onButtonPress={() => alert('button press')}
+        value={chatContent}
+        onChangeText={value => setChatContent(value)}
+        onButtonPress={chatSend}
       />
     </View>
   );
